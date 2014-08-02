8ac249c50741876693e90df18252a1432688a64a
lars berg <laserstorm@gmail.com>
2014-08-02 12:57:11 -0500

    fixed merge

--- src/Tween.h
+++ src/Tween.h
@@ -10,6 +10,7 @@
 #include "Easings.h"
 #include "TweenEventArgs.h"
 
+
 #include <iostream>     // std::cout
 
 namespace Tween
@@ -340,7 +341,6 @@ namespace Tween
 		T value;
 		float (*ease)(float);
 	};
-<<<<<<< HEAD
     
 	
 	
@@ -452,66 +452,8 @@ namespace Tween
 			return value;
 		}
 	};
-	
-	
-	/**
-	 *
-	 * Tween manger for adding, updating and removing tweens
-	 *
-	 */
-	class TweenManager
-	{
-	public:
-		TweenManager();
-		
-		~TweenManager();
-		
-		void clear();
-		
-		void update(ofEventArgs& e);
-		
-		void update( float t = ofGetElapsedTimeMillis() );
-        
-        template<class T>
-        static Tween* makeTween( T* target, T startVal, T endVal, float duration, float delay=0, float (*ease)(float) = Ease::Linear ){
-			auto t = new TweenItem<T>(target, startVal, endVal, delay, duration);
-			t->setEase(ease);
-            return t;
-        };
-		
-		//adding existing tweens
-		Tween* addTween(Tween* t){
-            tweens.push_back(t);
-            t->added(ofGetElapsedTimeMillis());
-            return t;
-        }
-        
-        template<class T>
-        Tween* addTween( T* target, T startVal, T endVal, float duration, float delay=0, float (*ease)(float) = Ease::Linear ){
-            Tween* t = TweenManager::makeTween(target, startVal, endVal, duration, delay, ease);
-            return addTween(t);
-        }
-        
-        template<class T>
-        Tween* addTween( T* target, T startVal, T endVal, float duration, float (*ease)(float) ){
-            Tween* t = TweenManager::makeTween(target, startVal, endVal, duration, 0, ease);
-            return addTween(t);
-        }
-		
-		//removing tweens
-		void remove(Tween* t, bool bDelete = true);
-		
-		//getting tweens by target
-		Tween* getTween(void* target);
-		
-	private:
-		
-		// tween vectors
-		vector< Tween* > tweens;
-	};
-}
-=======
 }
 
+
+
 #include "TweenManager.h"
->>>>>>> pr/3
